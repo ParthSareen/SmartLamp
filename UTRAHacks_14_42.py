e81d86327b94e535669626e0679498c3cc7f5333
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2018-11-03 21:01:42 -0400

    Add files via upload

--- UTRAHacks_14_42.py
+++ UTRAHacks_14_42.py
@@ -1,8 +1,32 @@
 import serial
 import time
 import math
+import json
 
-arduino = serial.Serial('COM23', 115200)
+##arduino = serial.Serial('COM24', 115200)
+jsonEx = '{"position":[-138.407,72.3262,9.48292],"pinch":0.2}'
+
+def get3DPoint(jsonInput):
+    try:
+        decoded = json.loads(jsonInput)
+        print(decoded)
+        print("JSON parsing example: ", decoded['position'][0],
+              decoded['position'][1],
+              decoded['position'][2])
+ 
+    except (ValueError, KeyError, TypeError):
+        print("JSON format error")
+
+def move3DPoint():
+    jsonInput = open('data.json', 'r')
+    decoded = json.load(jsonInput)
+    if decoded['pinch'] > 0.7:
+        print(decoded['position'][0], decoded['position'][1], decoded['position'][2])
+
+##        updatePosition(decoded['position'][0], decoded['position'][1], decoded['position'][2])
+
+get3DPoint(jsonEx)
+move3DPoint()
 
 horizontalOffset = 0
 verticalOffset = 0
@@ -34,7 +58,7 @@ def updatePosition(horizontal, vertical, depth):
     setX(xAngle)
     setY(yAngle)
 
-def updateFace(detect):
+def updateFace():
     updatePosition(0, -1, 1)
     setLed(64)
 
@@ -43,11 +67,8 @@ def setLed(pwm):
     print(command)
     arduino.write(command.encode())
 
-def resetPosition():
+def reset():
     updatePosition(0, 0, 1)
-
-def initialize():
-    resetPosition()
     setLed(255)
 
 def testLed():
